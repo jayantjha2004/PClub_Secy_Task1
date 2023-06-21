e2ad384910f497b51f75061d88e357ff440005f3
jayantjha2004 <132225118+jayantjha2004@users.noreply.github.com>
2023-06-21 23:50:07 +0530

    Create generator_test3.cpp

---
+++ generator_test3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //test 1
+    cout << 100000 << endl;
+    
+    for (int i = 1; i <= 100000; i++) {
+        cout << 1000000000 - i << endl;
+    }
+
+}
+
