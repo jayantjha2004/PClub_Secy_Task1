cb82dff8238a4a49c1dbc02db6488fd0d8ab1d6d
jayantjha2004 <132225118+jayantjha2004@users.noreply.github.com>
2023-06-21 23:47:53 +0530

    Create generator_sample.cpp

---
+++ generator_sample.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //sample generator
+    cout << 5 << endl;
+    int arr[] = {1, 2, 5, 8, 1000000000};
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << endl;
+    }
+
+}
+
